e68e82aee6cf03734714088ebfb0641417449aef
Brad Power <bpower@ccg.murdoch.edu.au>
2011-03-24 02:53:32 +0000

    jsonResponse no longer takes request/[] useless args

--- madas/registration/views.py
+++ madas/registration/views.py
@@ -25,6 +25,5 @@ def submit(request, *args):
         #setRequestVars(request, success=False, data = None, totalRows = 0, authenticated = True, authorized = True, mainContentFunction='error:existingRegistration')
 
         # TODO error type in mainContentFunction ??? WTF?
-        return jsonResponse(request, [], success=False, mainContentFunction='error:existingRegistration')
+        return jsonResponse( success=False, mainContentFunction='error:existingRegistration')
     print '*** registration/submit end ***' 
-    #return jsonResponse(request, [])
